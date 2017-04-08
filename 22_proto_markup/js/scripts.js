264804609118c15d0cb2cd2453db27b26fd65bbe
iBogdanov <ilya.bogdanov14@gmail.com>
2017-04-09 01:15:21 +0300

    Исправлена верстка и скрипт генерации сайта

--- 22_proto_markup/js/scripts.js
+++ 22_proto_markup/js/scripts.js
@@ -1,36 +1,19 @@
-      $(document).ready(function() {
-        $('[data-toggle="popover"]').popover({
-          html: 'true',
-          title: function() {
-              return $('<div align="center"><p>Ваш регион - <b>Новосибирск и область</b></p></div>');
-          },          
-          content: function() {
-              return $(
-              '<div align="center">' + 
-                '<p>Угадали?</p>' + 
-                  '<div class="btn-group" role="group" aria-label="...">' +
-                    '<button id="ok" type="button" class="btn btn-default">Да</button>' +
-                    '<button id="no" type="button" class="btn btn-default" data-toggle="modal" data-target="#RegionsModal">Нет</button>' +
-                  '</div>' +
-              '</div>'
-              );
-          },
-          placement: 'bottom',
-        });
+function setRegion(region)
+{
+  var selectedString = $(region).text();
+  $('.navbar').find('.popover-bottom').html('Регион: ' + selectedString + '<b class="caret"></b>');
+  $('.jumbotron').find('.dropdown-toggle').html('в ' + selectedString + '<span class="caret"></span>');
+  $('.table').find("[style='text-align:right;']").find('.dropdown-toggle').html(selectedString + '<span class="caret"></span>');
+  $('.modal').modal('hide');
+}
 
-        $('[data-toggle="popover"]').popover('show');
+function onRegionOk()
+{
+  setRegion($('.popover').find('b'));
+  $('.popover').remove();
+}
 
-        $('#ok').on('click', function() {
-            $("[data-toggle='popover']").popover("destroy");
-        });
-      });
-
-      function setRegion(region)
-      {
-        var RegionString = $(region).text();
-        $('#NavbarRegionString').html('Регион: ' + RegionString + '<b class="caret"></b>');
-        $('#InputFormRegionString').html('в ' + RegionString + '<span class="caret"></span>');
-        $('#AppsRegionString').html(RegionString + '<span class="caret"></span>');
-        $('#RegionsModal').modal('hide');
-        $('[data-toggle="popover"]').popover('destroy');
-      }
+function onRegionNo()
+{
+  $('.popover').remove();
+}
